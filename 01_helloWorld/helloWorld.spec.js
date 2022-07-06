cf0675eb87b4b6b504a3e9eee469c07ff3a11ef1
jodin22 <jeromekrwc@gmail.com>
2022-07-06 06:06:26 -0700

    edited semi-colons

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -3,5 +3,5 @@ const helloWorld = require('./helloWorld');
 describe('Hello World', function() {
   test('says "Hello, World!"', function() {
     expect(helloWorld()).toEqual('Hello, World!');
-  });
-});
+  })
+})
