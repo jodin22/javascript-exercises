ab370de8f9a766f5b55cc6f87a19f77e7381022f
jodin22 <jeromekrwc@gmail.com>
2022-07-23 19:10:06 -0700

    Add index and modified comments on sumAll HW

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
+/* 
 const sumAll = function() {
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+*/
+
+/* DO NOT DELETE THE ABOVE. RIGHT NOW YOU CAN'T USE JEST TO TEST YOUR CODE. SKIP THE JEST 
+PART AND TEST THINGS MANUALLY.  */
+
