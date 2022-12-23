3275f3c6f92dfe5e0ace5e2a7a64c3e45d312b29
jodin22sams <jeromekrwc@gmail.com>
2022-12-23 05:21:27 -0800

    fibonacci in progress. the loop works ok. it takes the first two numbers and creates the sequence and fills in a new array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -4,15 +4,17 @@ const fibonacci = function() {
 };
 */
 
-
-// create a series of numbers like this ex 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144..... 
+// create a series of numbers like this ex 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
+// 10946, 17711, 28657 
 // each number ( Fibonacci number ) is the sum of the two preceding numbers. 
 
 // fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
 // fibonacci(6) // returns 8
 
 /* 
-are 1st and 2nd always the same number?
+are 1st and 2nd always the same number? you don't need it to be the same. the loop at line 55 will take any 2 numbers and
+create a fibonacci sequence. ex, it can be 0, 1 or 1, 11 or 3, 8. it doesn't matter. the loop will calculate the sequence, but 
+you must provide the first two numbers for the const startFibArray.
 3rd = 1st + 2nd 
 4th = 2nd + 3rd
 5th = 3rd + 4th
@@ -26,16 +28,20 @@ and so on...
 
 */
 
-
 // add the 0 and 1 index to get a new number. this new number becomes index 2 and put it in the array using push.
 // now you have [1, 1, 2]
 // now add index 1 and 2 to get index 3. it will look like this after the push. [1, 1, 2, 3]
 // now add index 2 and index 3 to get index 4. it will look like this after the push [1, 1, 2, 3, 5]
 // keep going until you stop at a much later iteration, say 50?
 
-const startFibArray = [1, 1];
-const growFibArray = []; // put the 1, 1, here. then the results of each addition will be pushed to here as well. so it will 
-// then become 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 etc
+const startFibArray = [1, 1]; // this will hold the first two numbers. use split to put into the array that will hold the 
+// fibonacci numbers. split will not work bc that is for strings. use pop instead so the 0 index and 1 index will be put into 
+// the new array fillFibArray. pop will not work bc that only takes out the last element. use slice instead.
+
+const fillFibArray = startFibArray.slice(); // since no beginning and no end params are given, it starts at index 0 and goes 
+// to the end.
+console.log({fillFibArray}); // fillFibArray will be used to hold the results of the adding of the numbers to calculate the 
+// fibonacci sequence. use a loop using the approach at line 46
 
 // i = 0. i is index 0. then add i + 1 to get index 0 + 1 which becomes index 1. so the end of round 1 you added index 0 + 
 // index 1 which is a new value and that will be put into index 2. and the i++ happens so the next round is 
@@ -46,60 +52,17 @@ const growFibArray = []; // put the 1, 1, here. then the results of each additio
 // i = 2. now i is index 2. then add i + 1 to get index 2 + 1 which become index 3. so the end of round 3 you added index 2 + 
 // index 3 which is a new value and that will be put into index 4. and the i++ happens so the next round is...
 
-let initialIndexValue = 1;
-let nextIndexValue = 1;
-let newIndexValue = 0; 
-const fillFibArray = [];
-
-for (let i = 1; i <= 10; i++) { // keeps track of rounds. need some var declarations before the loop and inside the loop increase
-    // each inital, next and new by i or in reference to the previous
-   /* round 1
-let initialIndex = 1;
-let nextIndex = 1; this becomes initialIndex for next round 
-let newIndex = 2; // this becomes nextIndex for next round
-*/
-    console.log(`round ${i}: pushes ${initialIndexValue} and ${nextIndexValue} to the array`);
-    fillFibArray.push(initialIndexValue, nextIndexValue); // look at line 30 for more hints
-
-    // right now this is adding the same 1 over and over. need to adjust it so it increases each time instead of a constant 1.
-    // look at your palindromes loop for ex how each one increased by 1 and the other side decreased by 1. then it increased by 
-    // 2 and then decreased by 2. then increased by 3 and also decreased by 3 and so on. also look above at line 30 to 47. 
-    // that seems like a good idea to use for pushing new values into the array
+for (let i = 0; i <= 20; i++) { // the i is not only the counter but also used to get each index number.
+  const nextIndexValue = fillFibArray[i] + fillFibArray[i+1] // 0 is i. 1 is i+1. since this ex is only looking at the current
+  // index number and the next one up, the i and i+1 will work. and after each round, the i gets increased by 1 so then it 
+  // will be 1 and 1+1. then after the increase of 1, then it will be 2 and 2+1 and so on. this identifies each index you 
+  // want to add together and then push the result to the last place in the fillFibArray. this array will keep filling in with 
+  // the rest of the fibonacci sequence.
+  console.log(`index ${i}: ${fillFibArray[i]} plus index ${i+1}: ${fillFibArray[i+1]} = ${nextIndexValue}`);
+  fillFibArray.push(nextIndexValue);
+  console.log(`${nextIndexValue} is pushed to the new array`);
+  console.log({fillFibArray});
 };
-console.log({fillFibArray});
-/* round 2 
-initialIndex = 1
-nextIndex = 2; this becomes initialIndex for next round
-newIndex = 3; this becomes nextIndex for next round
-*/
-
-/* round 3
-initialIndex = 2;
-nextIndex = 3; this becomes initialIndex for next round 
-newIndex = 5; this becomes nextIndex for next round 
-
-*/
-
-/*  
-initialIndex = 3
-nextIndex = 5; this becomes initialIndex for next round 
-newIndex = 8; this becomes nextIndex for next round  
-
-*/
-
-/*  
-// after the 0 part, the below is for any other integer
-  const integerFillArray = [];  // an empty array which will be filled in by the loop
-  console.log(`starting array: ${integerFillArray}`);
-    for(let i = 1; i <= number; i++) {  // the counter will go from 1 to n. where n is the actual number so it counts up by 1
-      console.log(`iteration ${i}: adds ${i} to the array`); // shows each integer count
-      integerFillArray.push(i);  // at each integer, it pushes that value into the array so for 5, it will push in 1, 2, 3, 4, 5
-    };
-  console.log(`ending array: ${integerFillArray}`);  // shows the array with the integers. now that it is in an array, you can
-  // use reduce to multiply them all together (see line 153 for the previous multiply function). this may work for most cases, but
-  // you will need a separate part of 0 bc 0 factorial is 1.
-
-*/
 
 const fibonacci = function() {
 
