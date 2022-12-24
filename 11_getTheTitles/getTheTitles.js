fdf2bb6bcb91a2bf5b689d42c9cc7c3456e7dee5
jodin22sams <jeromekrwc@gmail.com>
2022-12-24 05:32:07 -0800

    comments to refer to JavaScript30 for array cardio day 1 and day 2 since they have good ex's of arrays containing elements that are objects.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -2,5 +2,29 @@ const getTheTitles = function() {
 
 };
 
+// look at ~/repos/JavaScript30/'04 - Array cardio day 1 and '07 - Array cardio day 2.
+// these 2 hw's have ex's of an array where each element is an object.
+
+/* 
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+
+];
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
+
+*/
+
 // Do not edit below this line
 module.exports = getTheTitles;
