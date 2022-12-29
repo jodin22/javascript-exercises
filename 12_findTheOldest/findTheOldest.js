0074819049fdcd87123eef40b08c545cc5a31ffd
jodin22sams <jeromekrwc@gmail.com>
2022-12-29 09:40:28 -0800

    findTheOldest in progress. map seems to work ok. sort seems to work  ok. next is to try using date function with sort.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -83,9 +83,49 @@ const findEachPersonAgeTest1 = peopleTest1.map((element, index, array) => {
     nameOfPerson = element.name; 
     console.log(nameOfPerson); 
     ageOfPerson = element.yearOfDeath - element.yearOfBirth; // each iteration shows the age of that person
-    console.log(`${nameOfPerson} is ${ageOfPerson}`);
-    
+    console.log(`${nameOfPerson} is ${ageOfPerson}`); // shows person's age in a sentence
+    const results = `${nameOfPerson} is ${ageOfPerson}`;
+    console.log(results);
+    return results;
 });
+
+console.log(findEachPersonAgeTest1);
+
+
+console.log(`original array: ${JSON.stringify(peopleTest1)}`); // shows the original order of the objects
+
+const comparePersonAgeTest1 = peopleTest1.sort((initialPerson, nextPerson) => { // each param is the individual object in the array.
+    // it needs to be listed twice when you want to do a sort bc it has to compare two things
+    let ageOfInitialPerson = 0;
+    let ageOfNextPerson = 0;
+    ageOfInitialPerson = initialPerson.yearOfDeath - initialPerson.yearOfBirth; // this is the age of the initial object
+    ageOfNextPerson = nextPerson.yearOfDeath - nextPerson.yearOfBirth; // this is the age of the next object
+        
+    if (ageOfInitialPerson > ageOfNextPerson) { // this is where the comparision happens. when the initial object is greater than
+        return -1; // the next object, then it gets pushed to the left of the sorted results. 
+    } else {
+        return 1; // if it is not greater than, it gets pushed to the right. 
+    };  
+}); // think of it as each object spread out in a horizontal line and the sort compares one with the next. the greater one gets 
+// sent to the left. then it keeps the greater one and compares with the next one and whichever is greater is kept and pushed
+// to the left which means the lesser ones get pushed further down the horizontal list.
+// this goes through all the objects in the array and keeps "bubbling" up and down the list to show greatest to smallest.
+
+// the default for sort is it sorts it inside the original array. so if you were to console.log the array again, it will show
+// the objects have changed positions aka index numbers. whereas map will give you a new copy aka shallow copy of the array.
+
+console.log(`sorted array by oldest: ${JSON.stringify(comparePersonAgeTest1)}`); // this is actually the original array presented in 
+// a new order
+
+// to show the highest number, you need to reference just the index number of 0 bc after the sort, the new 0 is the highest 
+// number instead of the original index 0 which was carly
+
+console.log(`the oldest person: ${JSON.stringify(comparePersonAgeTest1[0])}`);
+
+// this next part is adding a test to find the oldest person if someone is still living. this is where you will use the date 
+// function to see if they are still alive as of today's date?
+
+
 
 
 
